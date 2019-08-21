a5ee00895da3b3bf480b497a3d988aa944113f5b
dhoriachko <dhoriachko@n-ix.com>
2019-08-21 18:06:58 +0300

    first commit

---
+++ components/Banner.js
@@ -0,0 +1,18 @@
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Book club</h1>
+            </header>
+            <div className="content">
+                <p>A responsive site template designed by HTML5 UP<br />
+                and released under the Creative Commons.</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
